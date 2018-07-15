fe8050d9c21843c0249ea4b4865adc6e82ab9609
i3water <121024123@qq.com>
2018-07-15 21:04:43 +0800

    update codes & readme, fix PROGMEM bug

---
+++ AQI_Detector/AQI_Detector_main.cpp
@@ -0,0 +1,197 @@
+/* 
+ * BLINKER_PRO is use for professional device
+ * 
+ * Please make sure you have permission to modify professional device!
+ * Please read usermanual first! Thanks!
+ * https://doc.blinker.app/
+ * 
+ * Written by i3water for blinker.
+ * Learn more:https://blinker.app/
+ */
+
+#define BLINKER_PRINT Serial
+#define BLINKER_PRO
+#define BLINKER_DEBUG_ALL
+
+#define BLINKER_BUTTON
+#define BLINKER_BUTTON_PIN D7
+
+#include <Blinker.h>
+
+#include "AQI_Detector_main.h"
+#include "AQI_display.h"
+#include "AQI_sensor.h"
+
+static bool inited = false;
+static bool isAQI = true;
+static bool isReset = false;
+
+/* 
+ * Add your command parse code in this function
+ * 
+ * When get a command and device not parsed this command, device will call this function
+ */
+bool dataParse(const JsonObject & data)
+{
+    String getData;
+
+    data.printTo(getData);
+    
+    BLINKER_LOG2("Get user command: ", getData);
+    return true;
+}
+
+/* 
+ * Add your heartbeat message detail in this function
+ * 
+ * When get heartbeat command {"get": "state"}, device will call this function
+ * For example, you can print message back
+ * 
+ * Every 30s will get a heartbeat command from app 
+ */
+void heartbeat()
+{
+    Blinker.print("hello", "blinker");
+
+    BLINKER_LOG1("heartbeat!");
+}
+
+#if defined(BLINKER_BUTTON)
+/* 
+ * Blinker provide a button parse function for user if you defined BLINKER_BUTTON
+ * 
+ * Blinker button can detect singal click/ double click/ long press
+ * 
+ * Blinker.tick() will run by default, use interrupt will be better
+ */
+void buttonTick()
+{
+    Blinker.tick();
+}
+
+/* 
+ * Add your code in this function
+ * 
+ * When button clicked, device will call this function
+ */
+void singalClick()
+{
+    changeDetail();
+
+    if (inited) {
+        freshDisplay();
+    }
+
+    BLINKER_LOG1("Button clicked!");
+}
+
+/* 
+ * Add your code in this function
+ * 
+ * When button double clicked, device will call this function
+ */
+void doubleClick()
+{
+    changeMain();
+
+    if (inited) {
+        freshDisplay();
+    }
+
+    BLINKER_LOG1("Button double clicked!");
+}
+
+void longPressStart()
+{
+    isReset = true;
+    freshDisplay();
+
+    BLINKER_LOG1("Button long press start!");
+}
+#endif
+
+void AQI_init()
+{
+    Serial.begin(115200);
+
+    u8g2Init();
+    pmsInit();
+    
+    Blinker.begin(BLINKER_AIR_DETECTOR);
+
+    Blinker.attachParse(dataParse);
+    Blinker.attachHeartbeat(heartbeat);
+
+#if defined(BLINKER_BUTTON)
+    Blinker.attachClick(singalClick);
+    Blinker.attachDoubleClick(doubleClick);
+    Blinker.attachLongPressStart(longPressStart);
+    attachInterrupt(BLINKER_BUTTON_PIN, buttonTick, CHANGE);
+#endif
+
+    attachDisplay(display);
+}
+
+void AQI_run()
+{
+    Blinker.run();
+
+    aqiFresh();
+}
+
+void changeMain()
+{
+    isAQI = !isAQI;
+}
+
+void display()
+{
+    if (!isReset) {
+        if (isAQI) {
+            aqiDisplay(pm1_0Get(), pm2_5Get(), pm10_0Get(), humiGet(),
+                    hchoGet(), tempGet(), Blinker.hour(), Blinker.minute());
+        }
+        else {
+            timeDisplay(pm2_5Get(), Blinker.month(), Blinker.mday(), 
+                    Blinker.wday(), Blinker.hour(), Blinker.minute());
+        }
+    }
+    else {
+        resetDisplay();
+    }
+}
+
+bool checkInit()
+{
+    if (!inited) {
+        if (Blinker.inited()) {
+            setTimeLimit(1000UL);
+            inited = true;
+            return true;
+        }
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+void aqiFresh()
+{
+    if (!checkInit()) {
+        if (initDisplay()) {
+            Blinker.delay(5);
+        }
+        else if (isReset) {
+            freshDisplay();
+        }
+        else {
+            pmsFresh();
+            freshDisplay();
+        }
+    }
+    else {
+        pmsFresh();
+        freshDisplay();
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Blinker_PRO_AQI_Detector
 Open source AQI Detector, Blinker PRO device  
+
+# What's Blinker
+[Blinker](https://blinker.app/) is a platform with iOS and Android apps to control embedded hardware like Arduino. You can easily build graphic interfaces for all your projects by simply dragging and dropping widgets.  
+
+# Prerequisites
+You should have the following ready before beginning with any board:
+* [Arduino IDE](https://www.arduino.cc/en/Main/Software) 1.6.12 or later
+* Install the [esp8266/arduino](https://github.com/esp8266/arduino) core via the Arduino IDE Boards Manager
+* Install the [blinker-library](https://github.com/blinker-iot/blinker-library) via the Arduino Library Manager  
+* Install the [U8g2_Arduino](https://github.com/olikraus/U8g2_Arduino) via the Arduino Library Manager  
+* Install the [Adafruit_NeoPixel](https://github.com/adafruit/Adafruit_NeoPixel) via the Arduino Library Manager  
+
+
+# Hardware Needs
+Name | Quantity  
+- | -:  
+ESP8266 | 1  
+PMS5003ST | 1  
+SH1106 1.3' | 1  
+WS2812 | 1  
+Button | 1  
   
 Learn more: https://blinker.app  
